c97139bf4d28c60d1d66c08008faa93f74197816
Filip Dimitrov <github@filip-dimitrov.eu>
2020-09-09 17:04:36 +0300

    yolo

--- a.tf
+++ a.tf
@@ -1,6 +1,15 @@
-resource "null_resource" "exec8" {
+resource "null_resource" "exec9" {
 provisioner "local-exec" {
     command = "ls -laR /terraform"
+  }
+}
+
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "2.1.2"
+    }
   }
 }
 
